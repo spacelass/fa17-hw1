d7e10096576df2b9f753c6264536aae34e2a6d7d
Mumu Lin <mumulin@berkeley.edu>
2017-09-20 16:46:37 -0700

    HW1 complete

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,17 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    sum = 0
+    a.uniq!
+    while a.length>0 do 
+    	cur = a.shift
+    	if cur.to_i % 2 == 0
+    		if cur.to_i + 2 < 11
+    			sum += (cur.to_i + 2)
+    		end
+    	end
+	end
+    return sum
   end
+
 end
-
-
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,22 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  # accepts two integer parameters a and b. The method should calculate
+  # the sum of a and b and then return the square of the sum
+  return (a + b) * (a + b)
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  # accepts an array of integers as its parameter. The method should sort the given array
+  # increment each element by 1, and finally return it in a destructive or non-destructive fashion.
+  newArr = a.map { |a| 1+a }
+  return newArr.sort
 
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  # accepts two string parameters first_name and last_name. The method should return the
+  # first_name concatenated with the last_name with a space in between
+  return first_name + " " + last_name
 
 end
 
